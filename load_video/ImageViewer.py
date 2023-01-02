425638481c587411d11135c2202e8da14754c2a5
kmktu <kmktu@naver.com>
2023-01-02 14:28:47 +0900

    mod : Modify UI

--- load_video/ImageViewer.py
+++ load_video/ImageViewer.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import *
 class ImageViewer(QWidget): # 영상 frame viewer 클래스
     def __init__(self, parent=None):
         super(ImageViewer, self).__init__(parent)
-        self.setFixedSize(640, 360) # 640*360은 원본 영상 1920*1080의 1/3 사이즈
+        self.setFixedSize(620, 360) # 640*360은 원본 영상 1920*1080의 1/3 사이즈
         self.image = QImage()
         self.setAttribute(Qt.WA_OpaquePaintEvent)
 
